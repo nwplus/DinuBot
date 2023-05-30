5831925340157e611f1a8580bd1c397954303e8d
DonaldKLee <Donald3010@outlook.com>
2023-05-29 20:35:06 -0700

    set up bot

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("hello, world!")
+const { App } = require("@slack/bolt");
+
+
+require('dotenv').config();
+
+const botToken = process.env.token;
+const botSigningSecret = process.env.signingSecret;
+const botAppToken = process.env.appToken;
+
+const app = new App({
+    token: botToken, //Find in the Oauth  & Permissions tab
+    signingSecret: botSigningSecret, // Find in Basic Information Tab
+    socketMode:true,
+    appToken: botAppToken // Token from the App-level Token that we created
+});
+
+
+app.command("/test", async ({ command, say }) => {
+    try {
+        say("Hello")
+    } 
+    
+    catch (error) {
+        console.error(error);
+    }
+});
+
+app.start(3000)
